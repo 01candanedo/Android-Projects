c81a22c5a07ad41d8a9c432eb7d0f873a8a21c94
JxMark <rocadeuranio@outlook.com>
2023-04-18 19:48:58 -0500

    add some files

---
+++ InterfacePractice/app/src/main/java/com/example/interfazpractica1/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.interfazpractica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtDescripcion;
+    CheckBox chkServ, chkCom, chkFar;
+    RadioGroup rbgViable, rgbTiposRedes;
+    Spinner spnPresupuesto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        InicializarControles();
+    }
+
+    private void InicializarControles(){
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtDescripcion = (EditText) findViewById(R.id.txtDescripcion);
+
+        chkServ = (CheckBox) findViewById(R.id.chkMensaje);
+        chkCom = (CheckBox) findViewById(R.id.chkComida);
+        chkFar = (CheckBox) findViewById(R.id.chkFarmacia);
+
+        rbgViable = (RadioGroup) findViewById(R.id.rbgViable);
+
+        spnPresupuesto = (Spinner) findViewById(R.id.spnPresupuesto);
+    }
+
+    public void InterfazPrincipal(View v){
+        try {
+            String nombre = txtNombre.getText().toString();
+            String descripcion = txtDescripcion.getText().toString();
+            String servicios = ObtenerCheckbox();
+            String viable = "";
+
+            switch(rbgViable.getCheckedRadioButtonId()){
+                case R.id.rbtViableSi:
+                    viable = "Si";
+                    break;
+                case R.id.rbtViableNo:
+                    viable = "No";
+                    break;
+                default:
+                    viable = "Por favor vete";
+            }
+            Toast.makeText(this, "Datitos : "+nombre+" "+descripcion+" "+servicios+" "+viable, Toast.LENGTH_LONG).show();
+            new Mensajes("Titulo", "Mensaje", "Boton").show(getSupportFragmentManager(), "XD");
+        }catch (Exception e){
+            Toast.makeText(this, "Error brother: "+e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private String ObtenerCheckbox(){
+        String seleccion = "";
+
+        if(chkServ.isChecked()){
+            seleccion += chkServ.getText().toString()+", ";
+        }
+
+        if(chkFar.isChecked()){
+            seleccion += chkFar.getText().toString()+", ";
+        }
+
+        if(chkCom.isChecked()){
+            seleccion += chkCom.getText().toString()+", ";
+        }
+
+        return seleccion;
+    }
+
+    public void CargarSpinner(View view){
+        this.InicializarSpinner();
+    }
+
+    private List<String> ObtenerRedesSociales(){
+        List<String> redes = new ArrayList<String>();
+        redes.add("Twitch");
+        redes.add("You");
+        redes.add("Discord");
+        redes.add("Github");
+        redes.add("Gitlab");
+        return redes;
+    }
+
+    private void InicializarSpinner(){
+        ArrayAdapter<CharSequence> adapterPS5 = ArrayAdapter
+                .createFromResource(getApplicationContext(),R.array.juegosPS5,R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> adapterXBOX = ArrayAdapter
+                .createFromResource(getApplicationContext(),R.array.juegosXbox,R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> adapterSwitch =
+                new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,this.ObtenerJuegos());
+
+        switch (rgbTiposRedes.getCheckedRadioButtonId()){
+            case R.id.rbtSwitch:
+                spnJuegos.setAdapter(adapterSwitch);
+                break;
+            case R.id.rbtPS5:
+                spnJuegos.setAdapter(adapterPS5);
+                break;
+            case R.id.rbtXbox:
+                spnJuegos.setAdapter(adapterXBOX);
+                break;
+        }
+    }
+
+}
---
+++ InterfacePractice/app/src/main/java/com/example/interfazpractica1/Mensajes.java
@@ -0,0 +1,35 @@
+package com.example.interfazpractica1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class Mensajes extends DialogFragment {
+    private String Titulo, Boton, Mensaje;
+
+    public Mensajes(String titulo, String mensaje, String boton) {
+        Titulo = titulo;
+        Mensaje = mensaje;
+        Boton = boton;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder constructor = new AlertDialog.Builder(getActivity());
+        constructor.setTitle(Titulo)
+                .setMessage(Mensaje)
+                .setPositiveButton(Boton, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        return constructor.create();
+    }
+}
